a71fe4643f39b5b92561aece4e8693faf934d30d
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-01-31 20:14:39 +0900

    chore: next add unit test to make sure the logic
    and we need to add resize, swap, finally, move

--- src/main.rs
+++ src/main.rs
@@ -31,14 +31,17 @@ enum ElementMap<T: MapUnit = f32> {
     Window {
         id: Id,
         size_pos: SizeAndPos<T>,
+        percent: Size<f32>,
     },
     Vertical {
         elements: Vec<ElementMap<T>>,
         size_pos: SizeAndPos<T>,
+        percent: Size<f32>,
     },
     Horizontal {
         elements: Vec<ElementMap<T>>,
         size_pos: SizeAndPos<T>,
+        percent: Size<f32>,
     },
 }
 trait InsertCallback<T: MapUnit> {
@@ -58,6 +61,7 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
     fn new(size_pos: SizeAndPos<T>) -> Self {
         Self::EmptyOutput(size_pos)
     }
+
     fn id(&self) -> Option<Id> {
         match self {
             Self::Window { id, .. } => Some(*id),
@@ -72,28 +76,125 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
             | Self::EmptyOutput(size_pos) => *size_pos,
         }
     }
+
+    pub fn percent(&self) -> Size {
+        match self {
+            Self::Vertical { percent, .. }
+            | Self::Horizontal { percent, .. }
+            | Self::Window { percent, .. } => *percent,
+            Self::EmptyOutput(_) => Size::whole(),
+        }
+    }
+
     pub fn size(&self) -> Size<T> {
         self.size_pos().size
     }
 
     // NOTE: how to design it? what should I do with the size_pos? how does it mean?
     // maybe I need minus
-    fn expand<F>(&mut self, change: SizeAndPos<T>, f: &mut F)
+    fn expand<F>(&mut self, change: SizeAndPos<T>, diff_percent: Size, f: &mut F)
     where
         F: InsertCallback<T>,
     {
         match self {
             Self::EmptyOutput(_) => {}
-            Self::Window { size_pos, id } => {
+            Self::Window {
+                size_pos,
+                id,
+                percent,
+            } => {
                 *size_pos += change;
+                *percent += diff_percent;
                 f.callback(*id, *size_pos);
             }
-            _ => todo!(),
+            // NOTE: this time only change the percent of the top, and the size of the emelemts
+            Self::Vertical {
+                elements,
+                size_pos,
+                percent,
+            } => {
+                *size_pos += change;
+                *percent += diff_percent;
+                let container_height = size_pos.size.height;
+                let mut current_y = size_pos.position.y;
+
+                for element in elements {
+                    let percent_y = element.percent().height;
+                    // This will be the width
+                    let height = container_height.mul_f32(percent_y);
+
+                    let new_s_and_p: SizeAndPos<T> = SizeAndPos {
+                        size: Size {
+                            width: size_pos.size.width,
+                            height,
+                        },
+                        position: Position {
+                            x: size_pos.position.x,
+                            y: current_y,
+                        },
+                    };
+                    // Now we can caculte current height
+                    current_y += height;
+                    let s_and_p = element.size_pos();
+                    let diff_change = s_and_p - new_s_and_p;
+                    element.expand(diff_change, Size::zero(), f);
+                }
+            }
+            Self::Horizontal {
+                elements,
+                size_pos,
+                percent,
+            } => {
+                *size_pos += change;
+                *percent += diff_percent;
+                let container_width = size_pos.size.width;
+                let mut current_x = size_pos.position.x;
+
+                for element in elements {
+                    let percent_x = element.percent().width;
+                    // This will be the width
+                    let width = container_width.mul_f32(percent_x);
+
+                    let new_s_and_p: SizeAndPos<T> = SizeAndPos {
+                        size: Size {
+                            width,
+                            height: size_pos.size.height,
+                        },
+                        position: Position {
+                            x: current_x,
+                            y: size_pos.position.y,
+                        },
+                    };
+                    // Now we can caculte current height
+                    current_x += width;
+                    let s_and_p = element.size_pos();
+                    let diff_change = s_and_p - new_s_and_p;
+                    element.expand(diff_change, Size::zero(), f);
+                }
+            }
         }
     }
 
     pub fn width(&self) -> T {
         self.size().width
+    }
+
+    pub fn set_percentage(&mut self, c_percent: Size) {
+        match self {
+            Self::EmptyOutput(_) => {}
+            Self::Vertical { percent, .. }
+            | Self::Horizontal { percent, .. }
+            | Self::Window { percent, .. } => *percent = c_percent,
+        }
+    }
+
+    pub fn set_size_and_pos(&mut self, c_size_pos: SizeAndPos<T>) {
+        match self {
+            Self::EmptyOutput(size_pos)
+            | Self::Vertical { size_pos, .. }
+            | Self::Horizontal { size_pos, .. }
+            | Self::Window { size_pos, .. } => *size_pos = c_size_pos,
+        }
     }
 
     pub fn height(&self) -> T {
@@ -134,8 +235,9 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
             }
         }
     }
+
     #[must_use]
-    fn delete<F>(&mut self, target: Id, f: &mut F) -> bool
+    pub fn delete<F>(&mut self, target: Id, f: &mut F) -> bool
     where
         F: InsertCallback<T>,
     {
@@ -148,29 +250,49 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
         match self {
             Self::EmptyOutput(_) => false,
             // NOTE: this logic only comes when there is only one window exist
-            Self::Window { id, size_pos } => {
+            Self::Window { id, size_pos, .. } => {
                 if *id != target {
                     return false;
                 }
                 *self = Self::EmptyOutput(*size_pos);
                 true
             }
-            Self::Vertical { elements, .. } | Self::Horizontal { elements, .. } => {
+            Self::Vertical {
+                elements,
+                percent,
+                size_pos,
+            }
+            | Self::Horizontal {
+                elements,
+                percent,
+                size_pos,
+            } => {
                 let mut position: Option<usize> = None;
                 let mut window_s_a_p: Option<SizeAndPos<T>> = None;
+                // We use the percent to record how much the size is expand
+                // if it is +, means it expand
+                // else it shrink
+                let mut target_percent: Option<Size> = None;
                 for (index, element) in elements.iter_mut().enumerate() {
-                    if let Self::Window { id, size_pos } = element
+                    if let Self::Window {
+                        id,
+                        size_pos,
+                        percent,
+                    } = element
                         && *id == target
                     {
                         position = Some(index);
                         window_s_a_p = Some(*size_pos);
+                        target_percent = Some(*percent);
                         break;
                     }
                     if element.delete(target, f) {
                         return true;
                     }
                 }
-                let (Some(pos), Some(s_a_p)) = (position, window_s_a_p) else {
+                let (Some(pos), Some(s_a_p), Some(target_percent)) =
+                    (position, window_s_a_p, target_percent)
+                else {
                     return false;
                 };
                 let start = pos == 0;
@@ -179,7 +301,11 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
 
                 let element = &mut elements[adjust_pos];
                 let changed_size_and_pos = s_a_p.expend_change(expand_way);
-                element.expand(s_a_p, f);
+                element.expand(
+                    changed_size_and_pos,
+                    target_percent.change_expand(fit_way),
+                    f,
+                );
                 // NOTE: now we need a new function to expand the element.
                 // And we need a enum contains four fields
                 let _ = element;
@@ -188,7 +314,15 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
 
                 // it the element only one existed, downgrade it
                 if elements.len() == 1 {
+                    let o_percent = *percent;
+                    let o_size_pos = *size_pos;
+                    // first, we clone all the information in the element[0]
                     *self = elements[0].clone();
+                    // Since it means it replace all of the information, then the size and position
+                    // of container won't change
+                    // So we just give these to it
+                    self.set_percentage(o_percent);
+                    self.set_size_and_pos(o_size_pos);
                 }
 
                 true
@@ -215,32 +349,48 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
                 *self = Self::Window {
                     id,
                     size_pos: *size,
+                    percent: Size {
+                        width: 1.,
+                        height: 1.,
+                    },
                 };
                 true
             }
-            Self::Window { size_pos, id: o_id } => {
+            Self::Window {
+                size_pos,
+                id: o_id,
+                percent,
+            } => {
                 if *o_id != target {
                     return false;
                 }
                 let origin_size_pos = *size_pos;
                 let new_size_pos = size_pos.split(way);
+                let old_percent = *percent;
                 f.callback(*o_id, *size_pos);
                 f.callback(id, new_size_pos);
+                // NOTE: because it is will not become a window anymore, it will be the half of the
+                // Vertical or Horizontal
+                let new_percent = Size::whole().split(2., way);
+                *percent = new_percent;
                 let elements = vec![
                     self.clone(),
                     ElementMap::Window {
                         id,
                         size_pos: new_size_pos,
+                        percent: new_percent,
                     },
                 ];
                 *self = match way {
                     InsertWay::Vertical => ElementMap::Vertical {
                         elements,
                         size_pos: origin_size_pos,
+                        percent: old_percent,
                     },
                     InsertWay::Horizontal => ElementMap::Horizontal {
                         elements,
                         size_pos: origin_size_pos,
+                        percent: old_percent,
                     },
                 };
                 true
@@ -248,14 +398,21 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
             Self::Vertical { elements, .. } | Self::Horizontal { elements, .. } => {
                 let mut to_insert_index: Option<usize> = None;
                 let mut to_return: Option<SizeAndPos<T>> = None;
+                let mut new_percent: Option<Size> = None;
                 for (index, element) in elements.iter_mut().enumerate() {
-                    if let ElementMap::Window { id: o_id, size_pos } = element
+                    if let ElementMap::Window {
+                        id: o_id,
+                        size_pos,
+                        percent,
+                    } = element
                         && *o_id == target
                     {
                         if way == fit_way {
                             let new_size_pos = size_pos.split(way);
+                            *percent = percent.split(2., way);
                             to_return = Some(new_size_pos);
                             to_insert_index = Some(index);
+                            new_percent = Some(*percent);
                             break;
                         }
                         return element.insert(id, target, way, f);
@@ -265,9 +422,15 @@ impl<T: MinusAbleMatUnit> ElementMap<T> {
                         return insert_result;
                     }
                 }
-                if let (Some(index), Some(to_return)) = (to_insert_index, to_return) {
+                if let (Some(index), Some(to_return), Some(percent)) =
+                    (to_insert_index, to_return, new_percent)
+                {
                     let size_pos = to_return.clone();
-                    let window = Self::Window { id, size_pos };
+                    let window = Self::Window {
+                        id,
+                        size_pos,
+                        percent,
+                    };
                     elements.insert(index + 1, window);
                     return true;
                 }
@@ -297,10 +460,20 @@ fn main() {
             abc += 1;
         },
     );
-    println!("{abc}");
     dbg!(&element_map);
     let _ = element_map.insert(Id::unique(), Id(0), InsertWay::Vertical, &mut |id, size| {});
     dbg!(&element_map);
-    //element_map.insert(Id::unique(), Id(0), InsertWay::Horizontal);
-    //dbg!(&element_map);
+    let _ = element_map.insert(
+        Id::unique(),
+        Id(0),
+        InsertWay::Horizontal,
+        &mut |id, size| {},
+    );
+    dbg!(&element_map);
+
+    println!("=== delete ===");
+    let _ = element_map.delete(Id(0), &mut |id, size| {});
+    dbg!(&element_map);
+    let _ = element_map.delete(Id(1), &mut |id, size| {});
+    dbg!(&element_map);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,19 @@
 use std::iter::Sum;
-use std::ops::{Add, AddAssign, Div, Neg};
+use std::ops::{Add, AddAssign, Div, Neg, Sub, SubAssign};
 pub(crate) trait MapUnit:
-    Copy + Add<Output = Self> + Div<Output = Self> + Sum<Self>
+    Copy
+    + Add<Output = Self>
+    + AddAssign
+    + Sub<Output = Self>
+    + SubAssign
+    + Div<Output = Self>
+    + Sum<Self>
 {
     fn zero() -> Self;
     fn two() -> Self;
+    fn to_f32(&self) -> f32;
+    fn from_f32(val: f32) -> Self;
+    fn mul_f32(&self, val: f32) -> Self;
 }
 
 pub(crate) trait MinusAbleMatUnit: MapUnit + Neg<Output = Self> {}
@@ -17,6 +26,15 @@ macro_rules! impl_unit {
             }
             fn two() -> Self {
                 $value_2
+            }
+            fn to_f32(&self) -> f32 {
+                *self as f32
+            }
+            fn from_f32(val: f32) -> Self {
+                val as $Type
+            }
+            fn mul_f32(&self, val: f32) -> Self {
+                ((*self as f32) * val) as $Type
             }
         }
     };
@@ -31,7 +49,7 @@ impl_minus_able_unit!(f32, 0., 2.);
 impl_minus_able_unit!(i32, 0, 2);
 impl_unit!(u32, 0, 2);
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Size<T = f32> {
     pub width: T,
     pub height: T,
@@ -47,9 +65,85 @@ impl<T: MapUnit> Add for Size<T> {
     }
 }
 
+impl<T: MapUnit> Sub for Size<T> {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            width: self.width - other.width,
+            height: self.height - other.height,
+        }
+    }
+}
+
+impl<T: MapUnit> Size<T> {
+    pub fn split_h(&self, pieces: T) -> Self {
+        Self {
+            width: self.width / pieces,
+            ..*self
+        }
+    }
+    pub fn split_v(&self, pieces: T) -> Self {
+        Self {
+            height: self.height / pieces,
+            ..*self
+        }
+    }
+    pub fn split(&self, pieces: T, way: InsertWay) -> Self {
+        match way {
+            InsertWay::Horizontal => self.split_h(pieces),
+            InsertWay::Vertical => self.split_v(pieces),
+        }
+    }
+    pub fn percent_h(&self, percent: f32) -> Self {
+        Self {
+            width: self.width.mul_f32(percent),
+            height: self.height,
+        }
+    }
+    pub fn percent_v(&self, percent: f32) -> Self {
+        Self {
+            width: self.width,
+            height: self.height.mul_f32(percent),
+        }
+    }
+    pub fn percent(&self, percent: f32, way: InsertWay) -> Self {
+        match way {
+            InsertWay::Horizontal => self.percent_h(percent),
+            InsertWay::Vertical => self.percent_v(percent),
+        }
+    }
+}
+
+impl Size<f32> {
+    pub fn whole() -> Self {
+        Self {
+            width: 1.,
+            height: 1.,
+        }
+    }
+    /// This compute the change of the percent
+    pub fn change_expand(&self, way: InsertWay) -> Self {
+        match way {
+            InsertWay::Horizontal => Self {
+                width: self.width,
+                height: 0.,
+            },
+            InsertWay::Vertical => Self {
+                width: 0.,
+                height: self.height,
+            },
+        }
+    }
+}
+
 impl<T: MapUnit> AddAssign for Size<T> {
     fn add_assign(&mut self, rhs: Self) {
         *self = *self + rhs;
+    }
+}
+impl<T: MapUnit> SubAssign for Size<T> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
     }
 }
 
@@ -62,7 +156,7 @@ impl<T: MapUnit> Size<T> {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Position<T = f32> {
     pub x: T,
     pub y: T,
@@ -81,6 +175,21 @@ impl<T: MapUnit> Add for Position<T> {
 impl<T: MapUnit> AddAssign for Position<T> {
     fn add_assign(&mut self, rhs: Self) {
         *self = *self + rhs;
+    }
+}
+
+impl<T: MapUnit> Sub for Position<T> {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+impl<T: MapUnit> SubAssign for Position<T> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
     }
 }
 
@@ -111,6 +220,22 @@ impl<T: MapUnit> Add for SizeAndPos<T> {
 impl<T: MapUnit> AddAssign for SizeAndPos<T> {
     fn add_assign(&mut self, rhs: Self) {
         *self = *self + rhs;
+    }
+}
+
+impl<T: MapUnit> Sub for SizeAndPos<T> {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            size: self.size - rhs.size,
+            position: self.position - rhs.position,
+        }
+    }
+}
+
+impl<T: MapUnit> SubAssign for SizeAndPos<T> {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
     }
 }
 
